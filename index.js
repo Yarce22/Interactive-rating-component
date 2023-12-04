62ec43e48e83aa6f6ba69e19cd4f572163406e1c
Yarce22 <alejomira96@gmail.com>
2023-12-04 18:30:59 -0500

    Se crea la tarjeta de calificacion y se crean los eventos en JS

---
+++ index.js
@@ -0,0 +1,21 @@
+// Obtener todos los divs con la clase calification
+const calification = document.querySelectorAll('.calification');
+
+// Definir una función que cambia el color de fondo de un div
+function changeColor (div) {
+  // Recorrer todos los divs y ponerlos de color gris
+  for (let i = 0; i < calification.length; i++) {
+    calification[i].style.backgroundColor = '#262E38';
+    calification[i].style.color = '#7C8798';
+  }
+  // Cambiar el color de fondo del div seleccionado a naranja
+  div.style.backgroundColor = '#FC7614';
+  div.style.color = '#FFF';
+}
+
+// Agregar el evento click a cada div y llamar a la función changeColor
+for (let i = 0; i < calification.length; i++) {
+  calification[i].addEventListener('click', function () {
+    changeColor (calification[i]);
+  });
+}
